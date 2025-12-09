d59b8906ac8c1b3fca830db600b4420ba164ec7d
joao <jvictorbicker@gmail.com>
2025-12-09 02:01:41 -0300

    pagina login

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,22 @@ menuToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active');       // abre/fecha menu
     menuToggle.classList.toggle('active');    // transforma hambúrguer em X
 });
+
+const loginBtn = document.getElementById("loginBtn")!;
+const loginModal = document.getElementById("loginModal")!;
+const closeModal = document.getElementById("closeModal")!;
+
+loginBtn.addEventListener("click", () => {
+    loginModal.style.display = "flex"; // mostra modal
+});
+
+closeModal.addEventListener("click", () => {
+    loginModal.style.display = "none"; // esconde modal
+});
+
+// fecha ao clicar no fundo
+window.addEventListener("click", (event) => {
+    if (event.target === loginModal) {
+        loginModal.style.display = "none";
+    }
+});
